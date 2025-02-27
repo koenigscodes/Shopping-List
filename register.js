c22561840541a1a2b38e04a7f93457a8eee503f4
favour_koenig <favouruez11@gmail.com>
2025-02-27 16:19:57 +0100

    initial commit

---
+++ register.js
@@ -0,0 +1,52 @@
+
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+function addItem(e){
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+
+  //Validate Input
+  if (newItem === ""){
+    alert('Please a valid  item');
+    return;
+  }
+
+  //Create List Item
+  const li = document.createElement('li');
+  const textNode = document.createTextNode(newItem);
+  li.appendChild(textNode);
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+
+  itemInput.value = '';
+}
+
+function createButton(classes){
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon =createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes){
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+//Event Listeners
+itemForm.addEventListener('submit', addItem);
+
+
+
+
+
+
+
